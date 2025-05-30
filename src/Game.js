203fda60ca3ba0285ab82597cf2aebe24cf50eaa
NicolaManca <niko97.m@gmail.com>
2025-05-30 12:05:52 +0200

    Added difficult selection menu

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,9 @@
 import { useState, createContext } from "react";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import Board from "./Board";
 import "./Game.css";
 
 const sizes = {
-  test: 16,
   small: 64,
   medium: 256,
   large: 512,
@@ -16,13 +15,13 @@ const GameContext = createContext();
 function Game() {
   const [isGameOver, setIsGameOver] = useState(false);
   const [gameKey, setGameKey] = useState(uuidv4());
+  const [difficulty, setDifficulty] = useState("");
 
   function handleSquareClickGame(gameStatus) {
-    if(gameStatus === "lose"){
+    if (gameStatus === "lose") {
       setIsGameOver(true);
       console.log("GAME OVER - You clicked on a bomb!");
-    }
-    else if(gameStatus === "win"){
+    } else if (gameStatus === "win") {
       setIsGameOver(true);
       console.log("You won!");
     }
@@ -31,18 +30,57 @@ function Game() {
   const handleClickRestart = () => {
     setIsGameOver(false);
     setGameKey(uuidv4()); // Reset the game by generating a new key
-    console.log("Game restarted");
   };
 
+  const handleDifficultySelection = (size) => {
+    setDifficulty(sizes[size]);
+    setIsGameOver(false);
+    setGameKey(uuidv4()); // Reset the game by generating a new key
+  };
+
+  const handleClickMenu = () => {
+    setDifficulty("");
+  };
+
+  const difficulties = Object.keys(sizes).map((size) => {
+    return (
+      <button key={size} onClick={() => handleDifficultySelection(size)}>
+        {size.slice(0, 1).toUpperCase() + size.slice(1)}
+      </button>
+    );
+  });
+
   return (
-    
     <GameContext.Provider value={isGameOver}>
       <div className="game-header">
         <h1 className="game-title">Minesweeper</h1>
-        <button className="game-restart-button" onClick={handleClickRestart}>Restart</button>
+        {difficulty && (
+          <div className="game-header-buttons">
+            <button className="game-header-button" onClick={handleClickMenu}>
+              Menu
+            </button>
+            <button className="game-header-button" onClick={handleClickRestart}>
+              Restart
+            </button>
+          </div>
+        )}
         <h2 className="game-timer">Timer: TODO</h2>
       </div>
-      <Board key={gameKey} size={sizes.massive} handleSquareClick={handleSquareClickGame} />
+      {!difficulty && (
+        <div className="game-difficulty">
+          <h2>Select Difficulty:</h2>
+          <div className="game-difficulty-buttons">
+          {difficulties}
+          </div>
+        </div>
+      )}
+      {difficulty && (
+        <Board
+          key={gameKey}
+          size={difficulty}
+          handleSquareClick={handleSquareClickGame}
+        />
+      )}
     </GameContext.Provider>
   );
 }
